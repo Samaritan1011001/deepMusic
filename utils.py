ba8229124ce17c03d5eb50590f6277bc7ba3a4fb
Manoj <manojnb95@gmail.com>
2020-04-04 21:30:48 -0500

    Finishes a complete flow in cnn_model

--- utils.py
+++ utils.py
@@ -181,46 +181,6 @@ class Genres:
         return roots
 
 
-def load(filepath):
-
-    filename = os.path.basename(filepath)
-
-    if 'features' in filename:
-        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
-
-    if 'echonest' in filename:
-        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
-
-    if 'genres' in filename:
-        return pd.read_csv(filepath, index_col=0)
-
-    if 'tracks' in filename:
-        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1])
-
-        COLUMNS = [('track', 'tags'), ('album', 'tags'), ('artist', 'tags'),
-                   ('track', 'genres'), ('track', 'genres_all')]
-        for column in COLUMNS:
-            tracks[column] = tracks[column].map(ast.literal_eval)
-
-        COLUMNS = [('track', 'date_created'), ('track', 'date_recorded'),
-                   ('album', 'date_created'), ('album', 'date_released'),
-                   ('artist', 'date_created'), ('artist', 'active_year_begin'),
-                   ('artist', 'active_year_end')]
-        for column in COLUMNS:
-            tracks[column] = pd.to_datetime(tracks[column])
-
-        SUBSETS = ('small', 'medium', 'large')
-        tracks['set', 'subset'] = tracks['set', 'subset'].astype(
-                 CategoricalDtype(categories=SUBSETS, ordered=True))
-
-        COLUMNS = [('track', 'genre_top'), ('track', 'license'),
-                   ('album', 'type'), ('album', 'information'),
-                   ('artist', 'bio')]
-        for column in COLUMNS:
-            tracks[column] = tracks[column].astype('category')
-
-        return tracks
-
 # def load(filepath):
 #
 #     filename = os.path.basename(filepath)
@@ -234,155 +194,195 @@ def load(filepath):
 #     if 'genres' in filename:
 #         return pd.read_csv(filepath, index_col=0)
 #
-#     if 'subset_small' in filename:
+#     if 'tracks' in filename:
 #         tracks = pd.read_csv(filepath, index_col=0, header=[0, 1])
 #
-#         # COLUMNS = [('track_tags'), ('album_tags'), ('artist_tags'),
-#         #            ('track_genres'), ('track_genres_all')]
-#         # for column in COLUMNS:
-#         #     tracks[column] = tracks[column].map(ast.literal_eval)
+#         COLUMNS = [('track', 'tags'), ('album', 'tags'), ('artist', 'tags'),
+#                    ('track', 'genres'), ('track', 'genres_all')]
+#         for column in COLUMNS:
+#             tracks[column] = tracks[column].map(ast.literal_eval)
 #
-#         COLUMNS = [('track_date_created'), ('track_date_recorded'),
-#                    ('album_date_created'), ('album_date_released'),
-#                    ('artist_date_created'), ('artist_active_year_begin'),
-#                    ('artist_active_year_end')]
+#         COLUMNS = [('track', 'date_created'), ('track', 'date_recorded'),
+#                    ('album', 'date_created'), ('album', 'date_released'),
+#                    ('artist', 'date_created'), ('artist', 'active_year_begin'),
+#                    ('artist', 'active_year_end')]
 #         for column in COLUMNS:
 #             tracks[column] = pd.to_datetime(tracks[column])
 #
-#         SUBSETS = ('small_medium_large')
-#         tracks['set_subset'] = tracks['set_subset'].astype(
+#         SUBSETS = ('small', 'medium', 'large')
+#         tracks['set', 'subset'] = tracks['set', 'subset'].astype(
 #                  CategoricalDtype(categories=SUBSETS, ordered=True))
 #
-#         COLUMNS = [('track_genre_top'), ('track_license'),
-#                    ('album_type'), ('album_information'),
-#                    ('artist_bio')]
+#         COLUMNS = [('track', 'genre_top'), ('track', 'license'),
+#                    ('album', 'type'), ('album', 'information'),
+#                    ('artist', 'bio')]
 #         for column in COLUMNS:
 #             tracks[column] = tracks[column].astype('category')
 #
 #         return tracks
 
+def load(filepath):
 
-# def get_audio_path(audio_dir, track_id):
-#     tid_str = '{:06d}'.format(track_id)
-#     return os.path.join(audio_dir, tid_str[:3], tid_str + '.mp3')
-#
-#
-# class Loader:
-#     def load(self, filepath):
-#         raise NotImplemented()
-#
-#
-# class RawAudioLoader(Loader):
-#     def __init__(self, sampling_rate=SAMPLING_RATE):
-#         """
-#
-#         :rtype: object
-#         """
-#         self.sampling_rate = sampling_rate
-#         self.shape = (NB_AUDIO_SAMPLES * sampling_rate // SAMPLING_RATE, )
-#
-#     def load(self, filepath):
-#         return self._load(filepath)[:self.shape[0]]
-#
-#
-# class LibrosaLoader(RawAudioLoader):
-#     def _load(self, filepath):
-#         import librosa
-#         sr = self.sampling_rate if self.sampling_rate != SAMPLING_RATE else None
-#         # kaiser_fast is 3x faster than kaiser_best
-#         #x, sr = librosa.load(filepath, sr=sr, res_type='kaiser_fast')
-#         x, sr = librosa.load(filepath, sr=sr)
-#         return x
-#
-#
-# class AudioreadLoader(RawAudioLoader):
-#     def _load(self, filepath):
-#         import audioread
-#         a = audioread.audio_open(filepath)
-#         a.read_data()
-#
-#
-# class PydubLoader(RawAudioLoader):
-#     def _load(self, filepath):
-#         from pydub import AudioSegment
-#         song = AudioSegment.from_file(filepath)
-#         song = song.set_channels(1)
-#         x = song.get_array_of_samples()
-#         # print(filepath) if song.channels != 2 else None
-#         return np.array(x)
-#
-#
-# class FfmpegLoader(RawAudioLoader):
-#     def _load(self, filepath):
-#         """Fastest and less CPU intensive loading method."""
-#         import subprocess as sp
-#         command = ['ffmpeg',
-#                    '-i', filepath,
-#                    '-f', 's16le',
-#                    '-acodec', 'pcm_s16le',
-#                    '-ac', '1']  # channels: 2 for stereo, 1 for mono
-#         if self.sampling_rate != SAMPLING_RATE:
-#             command.extend(['-ar', str(self.sampling_rate)])
-#         command.append('-')
-#         # 30s at 44.1 kHz ~= 1.3e6
-#         proc = sp.run(command, stdout=sp.PIPE, bufsize=10**7, stderr=sp.DEVNULL, check=True)
-#
-#         return np.fromstring(proc.stdout, dtype="int16"), self.sampling_rate
+    filename = os.path.basename(filepath)
+
+    if 'features' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'echonest' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'genres' in filename:
+        return pd.read_csv(filepath, index_col=0)
+
+    if 'subset_small' in filename:
+        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1])
+
+        # COLUMNS = [('track_tags'), ('album_tags'), ('artist_tags'),
+        #            ('track_genres'), ('track_genres_all')]
+        # for column in COLUMNS:
+        #     tracks[column] = tracks[column].map(ast.literal_eval)
+
+        COLUMNS = [('track_date_created'), ('track_date_recorded'),
+                   ('album_date_created'), ('album_date_released'),
+                   ('artist_date_created'), ('artist_active_year_begin'),
+                   ('artist_active_year_end')]
+        for column in COLUMNS:
+            tracks[column] = pd.to_datetime(tracks[column])
+
+        SUBSETS = ('small_medium_large')
+        tracks['set_subset'] = tracks['set_subset'].astype(
+                 CategoricalDtype(categories=SUBSETS, ordered=True))
+
+        COLUMNS = [('track_genre_top'), ('track_license'),
+                   ('album_type'), ('album_information'),
+                   ('artist_bio')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].astype('category')
+
+        return tracks
 
 
-# def build_sample_loader(audio_dir, Y, loader):
-#
-#     class SampleLoader:
-#
-#         def __init__(self, tids, batch_size=4):
-#             self.lock1 = multiprocessing.Lock()
-#             self.lock2 = multiprocessing.Lock()
-#             self.batch_foremost = sharedctypes.RawValue(ctypes.c_int, 0)
-#             self.batch_rearmost = sharedctypes.RawValue(ctypes.c_int, -1)
-#             self.condition = multiprocessing.Condition(lock=self.lock2)
-#
-#             data = sharedctypes.RawArray(ctypes.c_int, tids.data)
-#             self.tids = np.ctypeslib.as_array(data)
-#
-#             self.batch_size = batch_size
-#             self.loader = loader
-#             self.X = np.empty((self.batch_size, *loader.shape))
-#             self.Y = np.empty((self.batch_size, Y.shape[1]), dtype=np.int)
-#
-#         def __iter__(self):
-#             return self
-#
-#         def __next__(self):
-#
-#             with self.lock1:
-#                 if self.batch_foremost.value == 0:
-#                     np.random.shuffle(self.tids)
-#
-#                 batch_current = self.batch_foremost.value
-#                 if self.batch_foremost.value + self.batch_size < self.tids.size:
-#                     batch_size = self.batch_size
-#                     self.batch_foremost.value += self.batch_size
-#                 else:
-#                     batch_size = self.tids.size - self.batch_foremost.value
-#                     self.batch_foremost.value = 0
-#
-#                 # print(self.tids, self.batch_foremost.value, batch_current, self.tids[batch_current], batch_size)
-#                 # print('queue', self.tids[batch_current], batch_size)
-#                 tids = np.array(self.tids[batch_current:batch_current+batch_size])
-#
-#             print(tids)
-#             for i, tid in enumerate(tids):
-#                 self.X[i] = self.loader.load(get_audio_path(audio_dir, tid))
-#                 self.Y[i] = Y.loc[tid]
-#
-#             with self.lock2:
-#                 while (batch_current - self.batch_rearmost.value) % self.tids.size > self.batch_size:
-#                     # print('wait', indices[0], batch_current, self.batch_rearmost.value)
-#                     self.condition.wait()
-#                 self.condition.notify_all()
-#                 # print('yield', indices[0], batch_current, self.batch_rearmost.value)
-#                 self.batch_rearmost.value = batch_current
-#
-#                 return self.X[:batch_size], self.Y[:batch_size]
-#
-#     return SampleLoader
+def get_audio_path(audio_dir, track_id):
+    tid_str = '{:06d}'.format(track_id)
+    return os.path.join(audio_dir, tid_str[:3], tid_str + '.mp3')
+
+
+class Loader:
+    def load(self, filepath):
+        raise NotImplemented()
+
+
+class RawAudioLoader(Loader):
+    def __init__(self, sampling_rate=SAMPLING_RATE):
+        """
+
+        :rtype: object
+        """
+        self.sampling_rate = sampling_rate
+        self.shape = (NB_AUDIO_SAMPLES * sampling_rate // SAMPLING_RATE, )
+
+    def load(self, filepath):
+        return self._load(filepath)[:self.shape[0]]
+
+
+class LibrosaLoader(RawAudioLoader):
+    def _load(self, filepath):
+        import librosa
+        sr = self.sampling_rate if self.sampling_rate != SAMPLING_RATE else None
+        # kaiser_fast is 3x faster than kaiser_best
+        #x, sr = librosa.load(filepath, sr=sr, res_type='kaiser_fast')
+        x, sr = librosa.load(filepath, sr=sr)
+        return x
+
+
+class AudioreadLoader(RawAudioLoader):
+    def _load(self, filepath):
+        import audioread
+        a = audioread.audio_open(filepath)
+        a.read_data()
+
+
+class PydubLoader(RawAudioLoader):
+    def _load(self, filepath):
+        from pydub import AudioSegment
+        song = AudioSegment.from_file(filepath)
+        song = song.set_channels(1)
+        x = song.get_array_of_samples()
+        # print(filepath) if song.channels != 2 else None
+        return np.array(x)
+
+
+class FfmpegLoader(RawAudioLoader):
+    def _load(self, filepath):
+        """Fastest and less CPU intensive loading method."""
+        import subprocess as sp
+        command = ['ffmpeg',
+                   '-i', filepath,
+                   '-f', 's16le',
+                   '-acodec', 'pcm_s16le',
+                   '-ac', '1']  # channels: 2 for stereo, 1 for mono
+        if self.sampling_rate != SAMPLING_RATE:
+            command.extend(['-ar', str(self.sampling_rate)])
+        command.append('-')
+        # 30s at 44.1 kHz ~= 1.3e6
+        proc = sp.run(command, stdout=sp.PIPE, bufsize=10**7, stderr=sp.DEVNULL, check=True)
+
+        return np.fromstring(proc.stdout, dtype="int16"), self.sampling_rate
+
+
+def build_sample_loader(audio_dir, Y, loader):
+
+    class SampleLoader:
+
+        def __init__(self, tids, batch_size=4):
+            self.lock1 = multiprocessing.Lock()
+            self.lock2 = multiprocessing.Lock()
+            self.batch_foremost = sharedctypes.RawValue(ctypes.c_int, 0)
+            self.batch_rearmost = sharedctypes.RawValue(ctypes.c_int, -1)
+            self.condition = multiprocessing.Condition(lock=self.lock2)
+
+            data = sharedctypes.RawArray(ctypes.c_int, tids.data)
+            self.tids = np.ctypeslib.as_array(data)
+
+            self.batch_size = batch_size
+            self.loader = loader
+            self.X = np.empty((self.batch_size, *loader.shape))
+            self.Y = np.empty((self.batch_size, Y.shape[1]), dtype=np.int)
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+
+            with self.lock1:
+                if self.batch_foremost.value == 0:
+                    np.random.shuffle(self.tids)
+
+                batch_current = self.batch_foremost.value
+                if self.batch_foremost.value + self.batch_size < self.tids.size:
+                    batch_size = self.batch_size
+                    self.batch_foremost.value += self.batch_size
+                else:
+                    batch_size = self.tids.size - self.batch_foremost.value
+                    self.batch_foremost.value = 0
+
+                # print(self.tids, self.batch_foremost.value, batch_current, self.tids[batch_current], batch_size)
+                # print('queue', self.tids[batch_current], batch_size)
+                tids = np.array(self.tids[batch_current:batch_current+batch_size])
+
+            print(tids)
+            for i, tid in enumerate(tids):
+                self.X[i] = self.loader.load(get_audio_path(audio_dir, tid))
+                self.Y[i] = Y.loc[tid]
+
+            with self.lock2:
+                while (batch_current - self.batch_rearmost.value) % self.tids.size > self.batch_size:
+                    # print('wait', indices[0], batch_current, self.batch_rearmost.value)
+                    self.condition.wait()
+                self.condition.notify_all()
+                # print('yield', indices[0], batch_current, self.batch_rearmost.value)
+                self.batch_rearmost.value = batch_current
+
+                return self.X[:batch_size], self.Y[:batch_size]
+
+    return SampleLoader
