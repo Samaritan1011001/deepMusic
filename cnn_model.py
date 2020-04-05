af7b1cee3c258fd6803a218332de5c632b1bb556
Manoj <manojnb95@gmail.com>
2020-04-04 21:30:23 -0500

    Finishes a complete flow in cnn_model

--- cnn_model.py
+++ cnn_model.py
@@ -1,44 +1,193 @@
-
 import numpy as np
 import pandas as pd
-
-from python_speech_features import mfcc, logfbank
+from python_speech_features import mfcc
 import os
-
 import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+from sklearn.preprocessing import LabelEncoder, LabelBinarizer
+import utils
+from sklearn import preprocessing
+
+
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.compat.v1.Session(config=config)
 
-import tensorflow.keras as keras
-from tensorflow.keras import models
-from tensorflow.keras import layers
-from tensorflow.keras import optimizers
-from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder, LabelBinarizer, StandardScaler
+# print(keras.__version__)
 
-import utils
-from sklearn import preprocessing
+def main():
+    AUDIO_DIR = os.environ.get('AUDIO_DIR')
+
+    # Read data
+    tracks = pd.read_csv('fma_metadata/subset_small.csv', index_col=0)
+    print(tracks['set_split'].shape)
+    train = tracks.loc[tracks['set_split'] == 'training']
+    val = tracks.loc[tracks['set_split'] == 'validation']
+    test = tracks.index[tracks['set_split'] == 'test']
+    print(f'train -> {train.columns}')
+
+    # Check which genres are present
+    genres = list(LabelEncoder().fit(train['track_genre_top']).classes_)
+    print('Top genres ({}): {}'.format(len(genres), genres))
+
+    # one hot encode y
+    # labels_onehot = LabelBinarizer().fit_transform(tracks['track_genre_top'])
+    # labels_onehot = pd.DataFrame(labels_onehot, index=tracks.index)
+    # print(f'labels_onehot -> {labels_onehot.head()}')
+
+    labels_onehot = np.asarray(LabelEncoder().fit_transform(tracks['track_genre_top']))
+    labels_onehot = pd.DataFrame(labels_onehot, index=tracks.index)
+    print(f'labels_onehot -> {labels_onehot.head()}')
+    # print(f'label type {labels_onehot.shape}')
+
+    # For local training purposes
+    train = train.head(500)
+    val = val.head(500)
+    print(f'train.index.values -> {len(val.index.values)}')
+
+    # Input shape is this because 13->numcep in mfcc function and 9 -> number of frames considered
+    input_shape = (13, 9, 1)
+
+    # Network Architecture
+    tf.keras.backend.clear_session()
+
+    model = tf.keras.models.Sequential()
+
+    model.add(layers.Conv2D(16, (3, 3), activation='relu',
+                            strides=(1, 1), padding='same',
+                            input_shape=input_shape))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            strides=(1, 1), padding='same',
+                            ))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu',
+                            strides=(1, 1), padding='same',
+                            ))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu',
+                            strides=(1, 1), padding='same',
+                            ))
+    model.add(layers.MaxPool2D(2, 2))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(8, activation='softmax'))
+    model.summary()
+
+    # optimizer = optimizers.SGD(lr=0.1, momentum=0.9, nesterov=True)
+    optimizer = optimizers.Adam(lr=0.01)
+    model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # # Train and Validate
+    ff_loader = utils.FfmpegLoader()
+    # sample_loader = build_sample_loader(AUDIO_DIR, labels_onehot, ff_loader)
+    # bs = 5
+    # model.fit_generator(sample_loader(train.index.values, batch_size=bs), steps_per_epoch=len(train) // bs, epochs=2)
+    # sample_loader = build_sample_loader(AUDIO_DIR, labels_onehot, ff_loader)
+    # history = model.evaluate_generator(sample_loader(val.index.values, batch_size=bs), len(val.index.values) // bs)
+    # # loss = model.evaluate_generator(sample_loader(test, batch_size=10), test.size)
+    # print(history)
+
+    # METHOD 2 - DATAGENERATOR
+    # Parameters
+    params = {'dim': (13, 9),
+              'batch_size': 10,
+              'n_classes': 8,
+              'n_channels': 1,
+              'shuffle': True}
+
+    # Generators
+    training_generator = DataGenerator(train.index.values, labels_onehot,ff_loader, **params)
+    validation_generator = DataGenerator(val.index.values, labels_onehot,ff_loader, **params)
+
+    # Train model on dataset
+    model.fit_generator(generator=training_generator,
+                        validation_data=validation_generator,
+                        use_multiprocessing=True,
+                        workers=1)
+
+
+class DataGenerator(tf.keras.utils.Sequence):
+    'Generates data for Keras'
+
+    def __init__(self, list_IDs, labels, loader, batch_size=32, dim=(32, 32, 32), n_channels=1,
+                 n_classes=10, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+        self.loader = loader
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            signal, rate = self.loader.load(utils.get_audio_path(os.environ.get('AUDIO_DIR'), ID))
+            ran_index = np.random.randint(0, signal.shape[0] - int(rate / 10))
+            sample = signal[ran_index:ran_index + int(rate / 10)]
+            # print(f'shape of sample -> {sample.shape}')
+            normalized_X = preprocessing.normalize(mfcc(sample, rate, numcep=13, nfilt=26, nfft=1103).T)
+            temp = np.array(normalized_X)
+            rshaped_X = temp.reshape(temp.shape[0], temp.shape[1], 1)
+            X[i,] = rshaped_X
+
+            # Store class
+            y[i] = self.labels.loc[ID].to_numpy()
+
+        return X, tf.keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+
+
 def build_sample_loader(audio_dir, Y, loader):
-
     class SampleLoader:
         def __init__(self, tids, batch_size=4):
-#             self.lock1 = multiprocessing.Lock()
-#             self.lock2 = multiprocessing.Lock()
+
             self.batch_foremost = 0
-#             self.batch_rearmost = sharedctypes.RawValue(ctypes.c_int, -1)
-#             self.condition = multiprocessing.Condition(lock=self.lock2)
-#             data = sharedctypes.RawArray(ctypes.c_int, tids.data)
-#             self.tids = np.ctypeslib.as_array(data)
-            self.tids = np.asarray(tids.data)
-# #             print(f'self.tids-> {type(self.tids)}')
+            self.tids = np.asarray(tids)
             self.batch_size = batch_size
             self.loader = loader
             self.X = []
             self.Y = []
+
         def __iter__(self):
             return self
+
         def __next__(self):
-#             with self.lock1:
+            self.X = []
+            self.Y = []
             if self.batch_foremost == 0:
                 np.random.shuffle(self.tids)
             batch_current = self.batch_foremost
@@ -48,99 +197,22 @@ def build_sample_loader(audio_dir, Y, loader):
             else:
                 batch_size = self.tids.size - self.batch_foremost
                 self.batch_foremost = 0
-            tids = np.array(self.tids[batch_current:batch_current+batch_size])
-# #             print(tids)
+            tids = np.array(self.tids[batch_current:batch_current + batch_size])
             for i, tid in enumerate(tids):
-#                 self.X[i] = self.loader.load(get_audio_path(audio_dir, tid))
                 signal, rate = self.loader.load(utils.get_audio_path(audio_dir, tid))
-# #                 print(f'signal -> {signal.shape}')
-# #                 print(f'rate -> {rate}')
-# #                 print(f'signal rate -> {signal[:rate].shape}')
-# #                 print(f'self.x -> {self.X[i].shape}')
-# #                 print(f'mfcc -> {mfcc(signal[:rate],rate, numcep=13, nfilt=26, nfft=1103).T.shape}')
-                ran_index = np.random.randint(0,signal.shape[0]-int(rate/10))
-                sample = signal[ran_index:ran_index+int(rate/10)]
-                normalized_X = preprocessing.normalize(mfcc(sample,22050,numcep=13, nfilt=26, nfft=1103).T)
-# #                 print(f'norm x shape {normalized_X.shape}')
+                ran_index = np.random.randint(0, signal.shape[0] - int(rate / 10))
+                sample = signal[ran_index:ran_index + int(rate / 10)]
+                # print(f'shape of sample -> {sample.shape}')
+                normalized_X = preprocessing.normalize(mfcc(sample, rate, numcep=13, nfilt=26, nfft=1103).T)
                 self.X.append(normalized_X)
                 self.Y.append(Y.loc[tid])
             temp = np.array(self.X[:batch_size])
-# #             print(f'temp x shape {temp.shape}')
-            rshaped_X = temp.reshape(temp.shape[0],temp.shape[1],temp.shape[2],1)
-            return rshaped_X , np.array(self.Y[:batch_size])
+            rshaped_X = temp.reshape(temp.shape[0], temp.shape[1], temp.shape[2], 1)
+            return rshaped_X, np.array(self.Y[:batch_size])
+
     return SampleLoader
-AUDIO_DIR = os.environ.get('AUDIO_DIR')
-tracks = utils.load('fma_metadata/tracks.csv')
-features = utils.load('fma_metadata/features.csv')
-echonest = utils.load('fma_metadata/echonest.csv')
-np.testing.assert_array_equal(features.index, tracks.index)
-assert echonest.index.isin(tracks.index).all()
-# print(tracks.shape, features.shape, echonest.shape)
-
-
-subset = tracks.index[tracks['set', 'subset'] == 'small']
-assert subset.isin(tracks.index).all()
-assert subset.isin(features.index).all()
-features_all = features.join(echonest, how='inner').sort_index(axis=1)
-# print('Not enough Echonest features: {}'.format(features_all.shape))
-tracks = tracks.loc[subset]
-features_all = features.loc[subset]
-# print(tracks.shape, features_all.shape)
-
-
-train = tracks.index[tracks['set', 'split'] == 'training']
-val = tracks.index[tracks['set', 'split'] == 'validation']
-test = tracks.index[tracks['set', 'split'] == 'test']
-
-# print('{} training examples, {} validation examples, {} testing examples'.format(*map(len, [train, val, test])))
-
-genres = list(LabelEncoder().fit(tracks['track', 'genre_top']).classes_)
-#genres = list(tracks['track', 'genre_top'].unique())
-# print('Top genres ({}): {}'.format(len(genres), genres))
-genres = list(MultiLabelBinarizer().fit(tracks['track', 'genres_all']).classes_)
-# print('All genres ({}): {}'.format(len(genres), genres))
-labels_onehot = LabelBinarizer().fit_transform(tracks['track', 'genre_top'])
-labels_onehot = pd.DataFrame(labels_onehot, index=tracks.index)
-loader = utils.FfmpegLoader()
-SampleLoader = build_sample_loader(AUDIO_DIR, labels_onehot, loader)
-# print('Dimensionality: {}'.format(loader.shape))
-sample_X_shape = SampleLoader(train, batch_size=10).__next__()[0].shape
-# print(sample_X_shape)
-
-
-input_shape = (sample_X_shape[1],sample_X_shape[2],1)
-# print(input_shape)
-# keras.backend.clear_session()
-
-model = keras.models.Sequential()
-
-model.add(layers.Conv2D(16,(3,3),activation = 'relu',
-                        strides=(1,1), padding='same',
-                        input_shape=input_shape))
-model.add(layers.Conv2D(32,(3,3),activation = 'relu',
-                        strides=(1,1), padding='same',
-                        ))
-model.add(layers.Conv2D(64,(3,3),activation = 'relu',
-                        strides=(1,1), padding='same',
-                        ))
-model.add(layers.Conv2D(128,(3,3),activation = 'relu',
-                        strides=(1,1), padding='same',
-                        ))
-model.add(layers.MaxPool2D(2,2))
-model.add(layers.Dropout(0.5))
-model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(8, activation='softmax'))
-model.summary()
-
-optimizer = optimizers.SGD(lr=0.1, momentum=0.9, nesterov=True)
-model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
 
 
-model.fit_generator(SampleLoader(train, batch_size=10), steps_per_epoch=len(train) // 10, epochs=20)
-loss = model.evaluate_generator(SampleLoader(val, batch_size=10), val.size)
-# loss = model.evaluate_generator(SampleLoader(test, batch_size=10), test.size)
-
-# print(loss)
+if __name__ == '__main__':
+    main()
---
+++ pre-pro.py
@@ -0,0 +1,163 @@
+import utils
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_signals(signals):
+    fig, axes = plt.subplots(nrows=2, ncols=4, sharex=False,
+                             sharey=True, figsize=(20,5))
+    fig.suptitle('Time Series', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(4):
+            axes[x,y].set_title(list(signals.keys())[i])
+            axes[x,y].plot(list(signals.values())[i])
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_fft(fft):
+    fig, axes = plt.subplots(nrows=2, ncols=4, sharex=False,
+                             sharey=True, figsize=(20,4))
+    fig.suptitle('Fourier Transforms', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(4):
+            data = list(fft.values())[i]
+            Y, freq = data[0], data[1]
+            axes[x,y].set_title(list(fft.keys())[i])
+            axes[x,y].plot(freq, Y)
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_fbank(fbank):
+    fig, axes = plt.subplots(nrows=2, ncols=4, sharex=False,
+                             sharey=True, figsize=(20,4))
+    fig.suptitle('Filter Bank Coefficients', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(4):
+            axes[x,y].set_title(list(fbank.keys())[i])
+            axes[x,y].imshow(list(fbank.values())[i],
+                    cmap='hot', interpolation='nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def plot_mfccs(mfccs):
+    fig, axes = plt.subplots(nrows=2, ncols=4, sharex=False,
+                             sharey=True, figsize=(20,4))
+    fig.suptitle('Mel Frequency Cepstrum Coefficients', size=16)
+    i = 0
+    for x in range(2):
+        for y in range(4):
+            axes[x,y].set_title(list(mfccs.keys())[i])
+            axes[x,y].imshow(list(mfccs.values())[i],
+                    cmap='hot', interpolation='nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+def main():
+    # AUDIO_DIR = os.environ.get('AUDIO_DIR')
+    project_root = "/home/manojnb/deepMusic/"
+    tracks = utils.load(project_root + 'tracks.csv')
+
+    subset = tracks.index[tracks['set', 'subset'] == 'small']
+
+    assert subset.isin(tracks.index).all()
+    tracks = tracks.loc[subset]
+
+    train = tracks.index[tracks['set', 'split'] == 'training']
+    val = tracks.index[tracks['set', 'split'] == 'validation']
+    test = tracks.index[tracks['set', 'split'] == 'test']
+
+    print('{} training examples, {} validation examples, {} testing examples'.format(*map(len, [train, val, test])))
+
+    # genres = list(LabelEncoder().fit(tracks['track', 'genre_top']).classes_)
+    genres = list(tracks['track', 'genre_top'].unique())
+    print('Top genres ({}): {}'.format(len(genres), genres))
+
+    all_tracks = tracks.copy()
+    all_tracks.columns = ['_'.join(col).strip() for col in all_tracks.columns]
+    print(all_tracks.columns)
+
+
+    print(all_tracks.shape)
+
+
+    classes = list(np.unique(all_tracks['track_genre_top']))
+    print(classes)
+    class_dist = all_tracks.groupby(['track_genre_top'])['track_duration'].mean().dropna()
+    print(class_dist)
+
+
+    fig, ax = plt.subplots()
+    ax.set_title('Class distribution', y=1.08)
+    ax.pie(class_dist,labels=class_dist.index,autopct="%1.1f%%",shadow=False,startangle=90)
+    ax.axis('equal')
+    plt.savefig(project_root + "graphs/class_dist.png")
+    plt.show()
+    all_tracks.reset_index(inplace=True)
+
+
+    # def calc_fft(y, rate):
+    #     n = len(y)
+    #     freq = np.fft.rfftfreq(n, d=1 / rate)
+    #     Y = abs(np.fft.rfft(y) / n)
+    #     return (Y, freq)
+    #
+    # def envelope(y, rate, threshold):
+    #     mask = []
+    #     y = pd.Series(y).apply(np.abs)
+    #     y_mean = y.rolling(window=int(rate / 10), min_periods=1, center=True).mean()
+    #     for mean in y_mean:
+    #         if mean > threshold:
+    #             mask.append(True)
+    #         else:
+    #             mask.append(False)
+    #     return mask
+    #
+    # # Directory where mp3 are stored.
+    # AUDIO_DIR = os.environ.get('AUDIO_DIR')
+    # print(AUDIO_DIR)
+    #
+    # signals = {}
+    # fft = {}
+    # fbank = {}
+    # mfccs = {}
+    #
+    # for c in classes:
+    #     mp3_file = all_tracks[all_tracks.track_genre_top == c].iloc[0, 0]
+    #     signal, rate = utils.FfmpegLoader().load(utils.get_audio_path(AUDIO_DIR, mp3_file))
+    #     #     mask = envelope(signal, rate,0.5)
+    #     #     signal = signal[mask]
+    #     signals[c] = signal
+    #     fft[c] = calc_fft(signal, rate)
+    #
+    #     bank = logfbank(signal[:rate], rate, nfilt=26, nfft=1103).T
+    #     fbank[c] = bank
+    #     mel = mfcc(signal[:rate], rate, numcep=13, nfilt=26, nfft=1103).T
+    #     mfccs[c] = mel
+    #
+    # print(rate)
+    #
+    # plot_signals(signals)
+    # plt.savefig(project_root + "graphs/signals.png")
+    # plt.show()
+    #
+    # plot_fft(fft)
+    # plt.savefig(project_root + "graphs/fft.png")
+    # plt.show()
+    #
+    # plot_fbank(fbank)
+    # plt.savefig(project_root + "graphs/fbank.png")
+    # plt.show()
+    #
+    # plot_mfccs(mfccs)
+    # plt.savefig(project_root + "graphs/mfcc.png")
+    # plt.show()
+
+if __name__ == '__main__':
+    main()
